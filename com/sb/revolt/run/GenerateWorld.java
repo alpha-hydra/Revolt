85f7d399eea3d7d1bf2622480dbfb62530a69901
alpha-hydra <alphahydra@outlook.com>
2016-01-12 18:54:33 +0000

    Create GenerateWorld.java

---
+++ com/sb/revolt/run/GenerateWorld.java
@@ -0,0 +1,49 @@
+package com.sb.revolt.run;
+
+import com.sb.revolt.templates.Tile;
+
+public class GenerateWorld 
+{
+	
+	static int[] features = {3};
+	
+	public GenerateWorld()
+	{
+		
+	}
+	
+	@SuppressWarnings("unused")
+	public static void init()
+	{
+		Tile tile_1_1 = new Tile(1, 1, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_1_2 = new Tile(1, 2, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_1_3 = new Tile(1, 3, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_1_4 = new Tile(1, 4, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_1_5 = new Tile(1, 5, 1.0, 1.0, 1.0, false, 1, features);
+		
+		Tile tile_2_1 = new Tile(2, 1, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_2_2 = new Tile(2, 2, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_2_3 = new Tile(2, 3, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_2_4 = new Tile(2, 4, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_2_5 = new Tile(2, 5, 1.0, 1.0, 1.0, false, 1, features);
+		
+		Tile tile_3_1 = new Tile(3, 1, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_3_2 = new Tile(3, 2, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_3_3 = new Tile(3, 3, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_3_4 = new Tile(3, 4, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_3_5 = new Tile(3, 5, 1.0, 1.0, 1.0, false, 1, features);
+		
+		Tile tile_4_1 = new Tile(4, 1, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_4_2 = new Tile(4, 2, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_4_3 = new Tile(4, 3, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_4_4 = new Tile(4, 4, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_4_5 = new Tile(4, 5, 1.0, 1.0, 1.0, false, 1, features);
+		
+		Tile tile_5_1 = new Tile(5, 1, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_5_2 = new Tile(5, 2, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_5_3 = new Tile(5, 3, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_5_4 = new Tile(5, 4, 1.0, 1.0, 1.0, false, 1, features);
+		Tile tile_5_5 = new Tile(5, 5, 1.0, 1.0, 1.0, false, 1, features);
+	}
+
+}
